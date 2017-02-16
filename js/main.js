17cb8b2090abaab91fd6c6a663f28f0c6970613d
EdgardBF <chitoto1015@outlook.com>
2017-02-15 18:08:02 -0600

    Se creo la pagina index, solo para terminar de unir las partes del mismo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $( document ).ready(function(){
+    //Se inicializan los componentes
     $(".button-collapse").sideNav();
-    $('.slider').slider();
+    $('.slider').slider({
+        //Se le cambia el alto al Slider
+        height: 400
+    });
 })
 
 
